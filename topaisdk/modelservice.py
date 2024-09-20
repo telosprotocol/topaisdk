655ce52de080613fcceec584f1fcdaf9eb37508d
ErrorDing <error.ding@uptech.ai>
2024-09-20 17:09:41 +0800

    up log

--- topaisdk/modelservice.py
+++ topaisdk/modelservice.py
@@ -11,25 +11,26 @@ logger = logging.getLogger(DEFAULT_LOGGER_NAME)
 
 class ModelService:
     # Methods can only be decorat directly, and cannot be used to decorat @fastAPI.xx. This decorator will not be invoked
+    @staticmethod
     def consecutive_failure(_func = None, failures_num = DEFAULT_CONSECUTIVE_FAILURES_NUM) -> Callable:
         def decorator(method):
             is_async = inspect.iscoroutinefunction(method)
-            logger.info("method: {} is_async: {} failures_num: {}".format(method, is_async, failures_num))
+            logger.debug("method: {} is_async: {} failures_num: {}".format(method, is_async, failures_num))
             wrapper_func = None
             if is_async:
                 @wraps(method)
-                async def wrapper(*args, **kwargs):
-                    logger.info("async call: {} args: {} kwargs: {}".format(method, args, kwargs))
-                    result = await method(*args, **kwargs)
+                async def wrapper(self, *args, **kwargs):
+                    logger.debug("async call: {} args: {} kwargs: {} self:{} ray_check_health:{}".format(method, args, kwargs, self, self._ray_check_health))
+                    result = await method(self, *args, **kwargs)
                     return result
                 wrapper_func = wrapper
             else:
                 @wraps(method)
-                def wrapper(*args, **kwargs):
-                    logger.info("call: {} args: {} kwargs: {}".format(method, args, kwargs))
-                    result = method(*args, **kwargs)
+                def wrapper(self, *args, **kwargs):
+                    logger.debug("call: {} args: {} kwargs: {} self:{} ray_check_health:{}".format(method, args, kwargs, self, self._ray_check_health))
+                    result = method(self, *args, **kwargs)
                     return result
-                wrapper_func = wrapper            
+                wrapper_func = wrapper
             return wrapper_func
         return decorator(_func) if callable(_func) else decorator
 
