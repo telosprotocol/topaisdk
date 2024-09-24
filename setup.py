18f8ef2d54f283aa79c35a56bb84f322d527e6c1
ErrorDing <error.ding@uptech.ai>
2024-09-24 16:23:47 +0800

    update version to 0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="topaisdk",
-    version="0.1.1",
+    version="0.1.2",
     author="error.ding",
     author_email="error.ding@uptech.ai",
     description="topai python sdk",
